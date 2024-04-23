c391873d5d473b0c654df6d4172204a4e29e929b
EradicusMaximus <radiixx2202@gmail.com>
2024-04-23 19:22:03 +0200

    oppsie

--- src/game/Anticheat/WardenAnticheat/WardenWin.cpp
+++ src/game/Anticheat/WardenAnticheat/WardenWin.cpp
@@ -1290,7 +1290,7 @@ void WardenWin::Update()
             "INSERT INTO `system_fingerprint_usage` (`fingerprint`, `account`, `ip`, `realm`, `architecture`, `cputype`, `activecpus`, `totalcpus`, `pagesize`) "
             "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-        stmt.addUInt32(m_fingerprintSaved); // fingerprint not implemented
+        stmt.addUInt32(fingerprint); // fingerprint not implemented
         stmt.addUInt32(m_accountId);
         stmt.addString(m_sessionIP);
         stmt.addUInt32(realmID);
